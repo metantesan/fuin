84edc8207d525d7a849c69707cacf5b06e6a2898
hikari <me@metantesan.com>
2026-09-20 23:47:19 +0330

    feat(controller): initialize info tracing

--- crates/controller/src/main.rs
+++ crates/controller/src/main.rs
@@ -1,4 +1,9 @@
 #[tokio::main]
 async fn main() -> Result<(), fuin_controller::controller::Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .init();
+
     fuin_controller::controller::run().await
 }
